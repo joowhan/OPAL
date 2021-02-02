71b60a1434f88200942d56d7c6007efc66020cd7
somy-cd <22000646@handong.edu>
2021-02-02 16:52:51 +0900

    잘하셨습니다 최종

--- lib/screens/disabled/Aerobic_D_Goodjob.dart
+++ lib/screens/disabled/Aerobic_D_Goodjob.dart
@@ -15,17 +15,13 @@ class ADgoodjob extends StatelessWidget {
       children: <Widget>[
     Column(
         children: <Widget>[
-
-
           Text(
         '잘 하셨습니다!'
         ,
         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40)
           )
         ],
-    ), //하나의 함수로 묶어서 쓰면 좋다.
-        Padding(padding: EdgeInsets.all(20.0)),
-        Padding(padding: EdgeInsets.all(20.0)),
+    ),
       ],
     );
 
@@ -33,40 +29,62 @@ class ADgoodjob extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-    Column(
-    children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                  color: Colors.grey[200]
-              ),
-              borderRadius: BorderRadius.circular(15.0),
-              color: Colors.grey[200],
-            ),
-            margin: const EdgeInsets.all(50.0),
-            // padding: const EdgeInsets.all(0),
-            alignment: Alignment.center,
-            child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 1.0),
-                child: Text(
-              '''
-              
-      팔 위아래로 흔들기 X 5
-      바운스 X 10
-      팔 휘두르기 X 15
-      벽 밀기 X 5
-      노 젓기 X 10
-      숨 쉬기 X 5
-              ''',
-          style: Theme.of(context)
-              .textTheme
-              .headline6
-              .copyWith(color: Colors.black))),
+          Column(
+          children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.grey[200]
+                    ),
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.grey[200],
+                  ),
+                  margin: const EdgeInsets.all(50.0),
+                  child: Column(
+                    children: <Widget>[
+                    Text(
+    '''
+유산소
+'''    ,
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                        textAlign: TextAlign.left
+                    ),
+                    Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 50.0),
+                        alignment: Alignment.center,
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.schedule, size: 30),
+                            Text(
+                                ' 30분',
+                                style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                                textAlign: TextAlign.center
+                            )
+                          ]
+                        )
+                    ),
+                    Container(
+                      child: Text(
+    '''
+    
+팔 위아래로 흔들기 X 5 
+바운스 X 10
+팔 휘두르기 X 15
+벽 밀기 X 5
+노 젓기 X 10
+숨 쉬기 X 5
+
+    ''',
+                          style: TextStyle(fontSize: 20),
+                          textAlign: TextAlign.left
+                      )
+                    )
+                  ],
+                  )
+                )
+                // ),
+          ],
           ),
-    ],
-    ), //하나의 함수로 묶어서 쓰면 좋다.
-          Padding(padding: EdgeInsets.all(20.0)),
-          Padding(padding: EdgeInsets.all(20.0)),
         ],
     );
           var complete = Column(
@@ -99,30 +117,25 @@ class ADgoodjob extends StatelessWidget {
                     ),
                   ),
                 ],
-              ), //하나의 함수로 묶어서 쓰면 좋다.
-              Padding(padding: EdgeInsets.all(20.0)),
-              Padding(padding: EdgeInsets.all(20.0)),
+              ),
             ],
           );
     return Scaffold(
-    body: Center(
-    child: ListView(
-    children: <Widget>[
-      Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-      Image.asset('images/stars.png'),
-      Padding(padding: EdgeInsets.all(10.0)),
-      goodjob,
-      Padding(padding: EdgeInsets.all(10.0)),
-      rst,
-    Padding(padding: EdgeInsets.all(10.0)),
-    complete,
-    Padding(padding: EdgeInsets.all(10.0)),
-    ],
-    ),
-    ],
-    )),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset('images/stars.png'),
+                goodjob,
+                rst,
+                complete,
+              ],
+            ),
+          ],
+        )
+      ),
     );
   }
 }
